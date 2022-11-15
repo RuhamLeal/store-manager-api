f545fcab30ec1ec0883c4554daad771d73d94887
Ruham Leal <ruhamxlpro@hotmail.com>
2022-11-15 18:28:44 -0300

    feat: adding some models that do delete, update and delete a product

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -14,7 +14,28 @@ async function findProductById(productId) {
   return foundProduct;
 }
 
+async function registerProduct(product) {
+  await connection.execute(
+    'INSERT INTO StoreManager.products (name) VALUES (?)', [product],
+  );
+}
+
+async function updateProduct(productId, updatedProduct) {
+  await connection.execute(
+    'UPDATE StoreManager.products SET name = ? WHERE id = ?', [updatedProduct, productId],
+  );
+}
+
+async function deleteProduct(productId) {
+  await connection.execute(
+    'DELETE FROM StoreManager.products WHERE id = ?', [productId],
+  );
+}
+
 module.exports = {
   findAllProducts,
   findProductById,
+  registerProduct,
+  updateProduct,
+  deleteProduct,
 };
