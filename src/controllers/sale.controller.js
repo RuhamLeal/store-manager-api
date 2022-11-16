afe9efe3811f4cbc86c7f4ebdc33be548a031f26
Ruham Leal <ruhamxlpro@hotmail.com>
2022-11-16 13:51:09 -0300

    feat: adding sale controllers to find all sales and to find a sale by id, and their routes

--- src/controllers/sale.controller.js
+++ src/controllers/sale.controller.js
@@ -1,4 +1,15 @@
-const { registerSale } = require('../services/sale.service');
+const { registerSale, findAllSales, findSaleById } = require('../services/sale.service');
+
+const getSales = async (_req, res) => {
+  const { status, data } = await findAllSales();
+  return res.status(status).json(data);
+};
+
+const getSaleById = async (req, res) => {
+  const { id } = req.params;
+  const { status, data } = await findSaleById(id);
+  return res.status(status).json(data);
+};
 
 const addSale = async (req, res) => {
   const { status, data } = await registerSale(req.body);
@@ -7,4 +18,6 @@ const addSale = async (req, res) => {
 
 module.exports = {
   addSale,
+  getSaleById,
+  getSales,
 };
